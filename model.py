ab57441b34c9e9d64705529ab0674a06bd7d709a
zyayoung <1328410180@qq.com>
2022-10-31 15:55:03 +0800

    path

---
+++ model.py
@@ -0,0 +1,22 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+
+class Model(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.stem = nn.Linear(16*9, 256, bias=False)
+        self.v_proj = nn.Linear(9, 256)
+        self.gru = nn.GRUCell(256, 256)
+        self.fc = nn.Linear(256, 4, bias=False)
+        self.fc.weight.data.mul_(0.01)
+        self.drop = nn.Dropout()
+        self.history = []
+
+    def forward(self, x: torch.Tensor, v, hx=None):
+        x = F.max_pool2d(x, 10, 10)
+        x = (self.stem(x.flatten(1)) + self.v_proj(v)).relu()
+        x = self.drop(x)
+        hx = self.gru(x, hx)
+        return self.fc(self.drop(hx)).tanh(), hx
---
+++ test_path.py
@@ -0,0 +1,61 @@
+from matplotlib import pyplot as plt
+import torch
+
+import argparse
+
+from env_gl import Env
+from model import Model
+from ratation import _axis_angle_rotation
+
+# torch.set_grad_enabled(False)
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--resume')
+parser.add_argument('--demo', action='store_true')
+args = parser.parse_args()
+
+# model = Model()
+model = Model().eval()
+if args.resume:
+    model.load_state_dict(torch.load(args.resume, map_location='cpu'))
+env = Env(1, 'cpu')
+
+ctl_dt = 1 / 15
+
+real_traj = []
+nearest_pts = []
+env.reset()
+env.quad.v[:] = 0
+env.quad.v[:] = 0
+h = None
+loss_obj_avoidance = 0
+p_target = torch.tensor([30, 0, 0])
+for t in range(20*15):
+    color, depth, nearest_pt = env.render()
+    depth = torch.as_tensor(depth[:, None])
+    x = torch.clamp(1 / depth - 1, -1, 6)
+    target_v = p_target - env.quad.p
+    R = _axis_angle_rotation('Z',  env.quad.w[:, -1])
+    target_v_norm = torch.norm(target_v, 2, -1, keepdim=True)
+    target_v = target_v / target_v_norm * target_v_norm.clamp_max(6)
+    local_v = torch.squeeze(env.quad.v[:, None] @ R, 1)
+    local_v_target = torch.squeeze(target_v[:, None] @ R, 1)
+    state = torch.cat([
+        local_v,
+        env.quad.w,
+        local_v_target
+    ], -1)
+    act, h = model(x, state, h)
+    env.step(act, ctl_dt)
+    nearest_pts.append(nearest_pt[0].tolist())
+    x, y, z = env.quad.p[0].tolist()
+    real_traj.append([x, y, z])
+
+plt.figure(figsize=(3, 10))
+plt.scatter(p_target[1], p_target[0], marker='o', label='Target')
+x, y, _ = zip(*real_traj)
+plt.plot(y, x, label='Trajectory')
+x, y, _ = zip(*[p for p in nearest_pts if p[2] > 0])
+plt.scatter(y, x, marker='x', label='Obstacle', color='black')
+plt.legend()
+plt.savefig('demo.png')
